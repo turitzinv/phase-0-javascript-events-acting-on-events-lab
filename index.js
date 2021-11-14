670b2e7fdfc55acec89432303a26db9589ea6c0e
Victor Turitzin <turitzinv@gmail.com>
2021-11-14 12:59:35 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+/*dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+
+dodger.style.left = "0px";
+
+const dodger = document.getElementById("dodger");*/
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight () {
+      const rightNumbers = dodger.style.left.replace("px","");
+      const left = parseInt(rightNumbers, 10);
+
+      if (left < 360) {
+          dodger.style.left = `${left+1}px`
+      }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+        }
+    }
+  });
